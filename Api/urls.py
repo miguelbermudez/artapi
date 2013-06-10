b5bce9acec890c68e625d1908af8e3e15a2a035a
Miguel Bermudez <gmiggs@gmail.com>
2013-06-10 01:01:32 -0400

    color json view

--- Api/urls.py
+++ Api/urls.py
@@ -4,4 +4,6 @@ from Api import views
 urlpatterns = patterns('',
     #url(r'^$', views.index, name='index'),
     url(r'work/color$', views.color, name='color'),
+    url(r'work/j/color/$', views.colorj, name='color'),
+
 )
--- Api/views.py
+++ Api/views.py
@@ -1,4 +1,7 @@
 # Create your views here.
+import json
+from django.http import HttpResponse
+from django.core import serializers
 
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -8,6 +11,8 @@ import logging
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+JSONSerializer = serializers.get_serializer("json")
+json_serializer = JSONSerializer()
 
 
 
@@ -30,3 +35,18 @@ def color(request):
     return render(request, 'api/color.html', context)
 
 # Create your views here.
+def colorj(request):
+    page = request.GET.get('page')
+    paginator = Paginator(Work.objects.all(), 1000)
+
+    try:
+        works = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        works = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        works = paginator.page(paginator.num_pages)
+
+
+    return HttpResponse(json_serializer.serialize(works.object_list), mimetype="application/json")
